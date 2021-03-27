a44b46f22c414329ac98b75c34448445b4e9df2b
jackdot <80749905+jackdot@users.noreply.github.com>
2021-03-27 12:27:27 +0300

    Сделал 1, 2, 3, задания

--- src/geekbrains/lesson2/Main.java
+++ src/geekbrains/lesson2/Main.java
@@ -3,12 +3,14 @@ package geekbrains.lesson2;
 public class Main {
 
     public static void main(String args[]) {
-        invertArray1stWay();
+        invertArray();
         filledArray();
         arrayMultiplyBy2();
+        matrixDiagonalFill();
+        //arrayMinMax();
     }
 
-    public static void invertArray1stWay() {
+    public static void invertArray() {
         int[] arr = {0, 1, 0, 0, 1, 0, 0, 1};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
@@ -18,6 +20,7 @@ public class Main {
             }
             System.out.println(arr[i]);
         }
+        System.out.println();
     }
 
     public static void filledArray() {
@@ -28,21 +31,48 @@ public class Main {
             element += 3;
             System.out.println(array[i]);
         }
+        System.out.println();
     }
 
-    public static void arrayMultiplyBy2(){
+    public static void arrayMultiplyBy2() {
+
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] < 6){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
                 arr[i] *= 2;
-            } else{
+            } else {
                 arr[i] += 0;
             }
             System.out.println(arr[i]);
         }
-
+        System.out.println();
     }
+
+    public static void matrixDiagonalFill() {
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            int[] innerArray = matrix[i];
+
+            for (int j = 0; j < innerArray.length; j++) {
+                System.out.print(matrix[i][j] + "     ");
+
+            }
+
+            System.out.println();
+
+        }
+    }
+//    public static void arrayMinMax (){
+//        int[] array = new int[8];
+//        for (int i = 0; i < array.length; i++) {
+//
+//            System.out.println(array);
+//
+//        }
+//        System.out.println();
+//    }
 }
+
 
 
 
