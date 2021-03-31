b74fe99a51dc0ffe9b2b824d34334659e8cd2191
jackdot <80749905+jackdot@users.noreply.github.com>
2021-03-31 15:57:40 +0300

    !

--- src/geekbrains/lesson2/Main.java
+++ src/geekbrains/lesson2/Main.java
@@ -48,15 +48,19 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void matrixDiagonalFill() {
         int[][] matrix = new int[4][4];
-        int j = 0;
         for (int i = 0; i < matrix.length; i++) {
-            int[] innerArray = matrix[i];
+            matrix[i][i] = 1;
+            matrix[i][matrix.length - 1 - i] = 1;
+        }
 
-            for (j = 0; j < innerArray.length; j++) {
-
-                System.out.print(matrix[i][j] + "     ");
+            for (int[] ints : matrix) {
+                for(int anInt : ints) {
+                    System.out.print (anInt + " , ");
+                }
+                System.out.println();
 
             }
 
@@ -65,8 +69,8 @@ public class Main {
             System.out.println();
 
         }
-    }
 }
+
 
 
 
